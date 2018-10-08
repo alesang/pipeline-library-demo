a86605a958e4db8c7b78a4ba547f76f159b16d79
sduggisetty <subbaraoemail@gmail.com>
2018-10-08 11:33:29 -0400

    Update Collectors.groovy

--- vars/Collectors.groovy
+++ vars/Collectors.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(string name = "advi" ) {
+def call(String name = "advi" ) {
     def beatles = ["John","Paul","George","Subbu"]
     //for (int i=0;i < beatles.size();i++ ){ Or
     
-    echo 
+    echo "${name}"
     for (beatle in beatles) {
         def greeting = "Hello, "
     //println "$greeting" + "${beatles[i]}"  Or beatles[i]
